a4dcfaf52278f951f0e36a1053d31cae7b03577d
RavisherSinghGill <gill1122ravi@gmail.com>
2024-10-14 17:50:46 -0400

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SearchContextProvider } from "./context/SearchContext.tsx";
 
 // Instantiate the QueryClient
 const queryClient = new QueryClient({
@@ -19,9 +18,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <SearchContextProvider>
         <App />
-      </SearchContextProvider>
     </QueryClientProvider>
     <Toaster position="top-right" />
   </StrictMode>
